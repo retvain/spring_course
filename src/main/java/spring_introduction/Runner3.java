9be05ba77b91ac47a1a772c0258eac313a937bb3
Mikhail Pelagein <retvain@gmail.com>
2023-04-06 22:04:48 +0500

    init, test IoC

---
+++ src/main/java/spring_introduction/Runner3.java
@@ -0,0 +1,17 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Runner3 {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //Pet pet = context.getBean("myPet", Pet.class);
+//        Pet pet = new Dog();
+        Person person = context.getBean("myPerson", Person.class);
+        person.callYourPet();
+
+        context.close();
+    }
+}
